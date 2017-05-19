b03537b47e0aa4801b595af701137b1166611d16
Alexander Rogin <gonetc99@gmail.com>
2017-05-19 07:57:54 +0300

    class; video handling

--- vehicle_detector.py
+++ vehicle_detector.py
@@ -106,9 +106,52 @@ def getClassifier(train=False, color_space='RGB', spatial_size=(32, 32), hist_bi
 
     return model.load()
 
+class VehicleDetector:
+    def __init__(self, train):
+        ### TODO: Tweak these parameters and see how the results change.
+        # Color:
+        self.hist_feat = True # Histogram features on or off
+        self.color_space = 'HSV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+        self.hist_bins = 4    # Number of histogram bins (16)
+
+        # HOG
+        self.hog_feat = True # HOG features on or off
+        self.orient = 9  # HOG orientations (9)
+        self.pix_per_cell = 8 # HOG pixels per cell
+        self.cell_per_block = 2 # HOG cells per block
+        self.hog_channel = 2 # Can be 0, 1, 2, or "ALL"
+
+        # Spatial
+        self.spatial_feat = True # Spatial features on or off
+        self.spatial_size = (16, 16) # Spatial binning dimensions (16, 16)
+
+        if train:
+            self.X_scaler, self.svc = model.train(color_space=self.color_space, spatial_size=self.spatial_size, hist_bins=self.hist_bins, 
+                        orient=self.orient, pix_per_cell=self.pix_per_cell, cell_per_block=self.cell_per_block, 
+                        hog_channel=self.hog_channel, spatial_feat=self.spatial_feat, hist_feat=self.hist_feat, hog_feat=self.hog_feat)
+        else:
+            self.X_scaler, self.svc = model.load()
+
+    def processImage(self, image, pngJpg=True):
+        if pngJpg:
+            image = image.astype(np.float32)/255
+        windows = getWindows(image)
+        draw_image = np.copy(image)
+        hot_windows = search_windows(image, windows, self.svc, self.X_scaler, color_space=self.color_space, 
+                            spatial_size=self.spatial_size, hist_bins=self.hist_bins, 
+                            orient=self.orient, pix_per_cell=self.pix_per_cell, 
+                            cell_per_block=self.cell_per_block, 
+                            hog_channel=self.hog_channel, spatial_feat=self.spatial_feat, 
+                            hist_feat=self.hist_feat, hog_feat=self.hog_feat)
+
+        window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
+        return window_img
+
+
 
 image_path = 'test_images/test1.jpg'
 train = False
+useVideo = False
 
 argc = len(sys.argv)
 for i in range(argc):
@@ -117,52 +160,23 @@ for i in range(argc):
         image_path = sys.argv[i]
     elif sys.argv[i] == '--train':
         train = True
+    elif sys.argv[i] == '--video' and i < argc - 1:
+        i += 1
+        video_path = sys.argv[i]
+        useVideo = True
 
 
-### TODO: Tweak these parameters and see how the results change.
-# Color:
-hist_feat = True # Histogram features on or off
-color_space = 'HSV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-hist_bins = 4    # Number of histogram bins (16)
+vd = VehicleDetector(train)
 
-# HOG
-hog_feat = True # HOG features on or off
-orient = 9  # HOG orientations (9)
-pix_per_cell = 8 # HOG pixels per cell
-cell_per_block = 2 # HOG cells per block
-hog_channel = 2 # Can be 0, 1, 2, or "ALL"
+if not useVideo:
+    image = mpimg.imread(image_path)
+    
+    result = vd.processImage(image)
 
-# Spatial
-spatial_feat = True # Spatial features on or off
-spatial_size = (16, 16) # Spatial binning dimensions (16, 16)
-
-
-y_start_stop = [None, None] # Min and max in y to search in slide_window()
-
-
-X_scaler, svc = getClassifier(train=train, color_space=color_space, 
-                        spatial_size=spatial_size, hist_bins=hist_bins, 
-                        orient=orient, pix_per_cell=pix_per_cell, 
-                        cell_per_block=cell_per_block, 
-                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
-                        hist_feat=hist_feat, hog_feat=hog_feat)
-
-image = mpimg.imread(image_path)
-# !!!
-image = image.astype(np.float32)/255
-windows = getWindows(image)
-
-draw_image = np.copy(image)
-
-
-hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space, 
-                        spatial_size=spatial_size, hist_bins=hist_bins, 
-                        orient=orient, pix_per_cell=pix_per_cell, 
-                        cell_per_block=cell_per_block, 
-                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
-                        hist_feat=hist_feat, hog_feat=hog_feat)
-
-window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
-
-plt.imshow(window_img)
-plt.show()
+    plt.imshow(result)
+    plt.show()
+else:
+    from moviepy.editor import VideoFileClip
+    video = VideoFileClip(video_path)
+    processed_video = video.fl_image(vd.processImage)
+    processed_video.write_videofile('output.mp4', audio=False)
